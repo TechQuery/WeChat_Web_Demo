0536fc02624459c7b9554078b3956520299be19f
Tech_Query <shiy007@qq.com>
2016-10-24 19:25:19 +0800

    【新增】基于 EasyLibs.php 的后端接口

--- api.php
+++ api.php
@@ -2,14 +2,47 @@
 
 error_reporting(E_ALL ^ E_NOTICE);
 
-require_once('php/WeChat_JS_SDK.php');
 $_Config = require_once('php/config.php');
+
+
+/* ---------- 微信 SDK ---------- */
+
+require_once('php/WeChat_JS_SDK.php');
 
 $_JS_SDK = new WeChat_JS_SDK(
     'qyapi',  $_Config['JS-SDK']['AppID'],  $_Config['JS-SDK']['AppSecret']
 );
 
-$_Item = explode('?', $_SERVER['PATH_INFO'], 1);
-$_Item = substr($_Item[0], 1);
+/* ---------- RESTful API 服务器 ---------- */
 
-echo  $_GET['callback'] . '(' . json_encode( $_JS_SDK->$_Item ) . ');';
+require_once('php/EasyLibs.php');
+
+
+$_HTTP_Server = new HTTPServer(false,  function ($_Path) use ($_JS_SDK) {
+
+    $_Path = join('/', $_Path);
+
+    session_start();
+
+    if ($_Path == 'WeChat/signPackage') {
+        $_SESSION['UserId'] = $_JS_SDK->userInfo->UserId;
+        session_write_close();
+    } else
+        return  isset( $_SESSION['UserId'] );
+});
+
+
+/* ---------- 数据库对象 ---------- */
+
+$_Connection = $_Config['DataBase']['connection'];
+
+$_SQLDB = new MySQL(
+    $_Config['DataBase']['base_name'],  "{$_Connection[1]}:{$_Connection[2]}"
+);
+
+/* ---------- RESTful API 路由 ---------- */
+
+$_HTTP_Server->on('GET',  '/WeChat/',  function ($_Path) use ($_JS_SDK) {
+
+    return  $_GET['callback'] . '(' . json_encode( $_JS_SDK->{$_Path[1]} ) . ');';
+});
